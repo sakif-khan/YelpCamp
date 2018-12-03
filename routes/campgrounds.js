b91eff09629b005eaa27615b1fa3eb311ae05fc7
Md Sakif Khan <sakifkhan@iut-dhaka.edu>
2018-12-03 17:56:29 +0000

    add user information to campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,12 +14,15 @@ router.get("/", function(req, res){
 });
 
 // CREATE - add new campground to DB
-router.post("/", function(req, res){
-    // res.send("YOU HIT THE POST ROUTE!");
+router.post("/", isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: desc, author: author};
     
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
@@ -31,7 +34,7 @@ router.post("/", function(req, res){
 });
 
 // NEW - show form to create new DB
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
     res.render("campgrounds/new");
 });
 
@@ -45,5 +48,13 @@ router.get("/:id", function(req, res){
         }
     });
 });
+
+// middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
