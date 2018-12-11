647875b0a75db7294292724c0fd76b8d28ec438e
Md Sakif Khan <sakifkhan@iut-dhaka.edu>
2018-12-11 17:24:28 +0000

    add user profile

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-var express = require("express"),
-    router  = express.Router(),
-    passport= require("passport"),
-    User    = require("../models/user");
+var express     = require("express"),
+    router      = express.Router(),
+    passport    = require("passport"),
+    User        = require("../models/user"),
+    Campground  = require("../models/campground");
 
 // root route
 router.get("/", function(req, res){
@@ -15,10 +16,18 @@ router.get("/register", function(req, res){
 
 // handle sign up logic
 router.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
+    var newUser = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        avatar: req.body.avatar
+    });
+    
     if(req.body.adminCode === "secretcode123") {
         newUser.isAdmin = true;
     }
+    
     User.register(newUser, req.body.password, function(err, user){
         if(err){
             console.log(err);
@@ -49,6 +58,23 @@ router.get("/logout", function(req, res){
     req.logout();
     req.flash("success", "Logged you out!");
     res.redirect("/campgrounds");
+});
+
+// user profile
+router.get("/users/:id", function(req, res){
+    User.findById(req.params.id, function(err, foundUser){
+        if(err){
+            req.flash("error", "Something went wrong");
+            res.redirect("/");
+        }
+        Campground.find().where("author.id").equals(foundUser._id).exec(function(err, campgrounds){
+            if(err){
+                req.flash("error", "Something went wrong");
+                res.redirect("/");
+            }
+            res.render("users/show", {user: foundUser, campgrounds: campgrounds});
+        });
+    });
 });
 
 module.exports = router;
