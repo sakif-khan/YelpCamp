bd4d2d73986b04aa6876a29c927d699e640ebd66
Md Sakif Khan <sakifkhan@iut-dhaka.edu>
2018-12-11 10:16:06 +0000

    add dynamic price feature

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -17,13 +17,20 @@ router.get("/", function(req, res){
 // CREATE - add new campground to DB
 router.post("/", middleware.isLoggedIn, function(req, res){
     var name = req.body.name;
+    var price = req.body.price;
     var image = req.body.image;
     var desc = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     };
-    var newCampground = {name: name, image: image, description: desc, author: author};
+    var newCampground = {
+        name: name,
+        price: price,
+        image: image,
+        description: desc,
+        author: author
+    };
     
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
